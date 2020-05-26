fddfba1c9fca790a958c960280662d7cba2ee9b2
Rathetsu <ah.ezzat.muh@gmail.com>
2020-05-26 14:54:07 +0200

    solver algorithm

--- Solver.py
+++ Solver.py
@@ -1,81 +1,75 @@
 import numpy as np
 import SSR
+import LTISysSym as lti
 
-"""
-The Algorithm used to solve here is called the SS-SSEB Algorithm which is the most effiecient way for
-finding the numerical solution for the Single-Input-Single-Output-State-Space-Equation (SISO-SSE) using the Block method.
-
-"""
-
-def Fi(i, j):
-    # This function denotes the ith linear functional relationship.
-
-
-def __block(num, i , j):
-    Block_Result = Fi(i, j)
-    return Block_Result
-
-
-def SS_SSEB():
-    # Step 1: Define k and h
-    k = 0       # (k + 1) is the number of the time points.
-    h = (t_k - t_0) / k     # t_k is the last time point. t_0 is the initial state.
-
-    # Step 2: get the state space equation of the system 
+n = 3
+m = 3
+A = np.array([[0, 1, 0], [0, 0, 1], [-6, -11, -6]])
+B = np.array([[2], [-6], [16]])
+C = np.array([1, 0, 0])
+D = np.array([1])
+#Step 1:
+def get_h(k=1000, t_k=10.0, t_0=0.0):
+    """
+    :param t_k: Final time (set to 10 by default)
+    :param t_0: Initial time (set to 0 by default)
+    :param k: Total number of time steps excluding the last step. Total no. of time steps is (k+1)
+    :return: The factor h
+    """
+    h = (t_k - t_0)/k
+    return h
 
 
-    #Step 3:
-    # A list to store computational results of the block function.
-    results = [2][k]
+#Step 2:
 
-    for j in range(k):      # j is the number of time points.
-        for i in range(n):
+#Step 3:
+def f_B(B_i, A, B, x_temp, n):
+    #add a prameter to modify input u later
+    for i in range(0, n):  # Setting array B_1
+        B_i.append(A.dot(x_temp) + B)
 
 
 
 
 
 
+def block(n, u, k=1000):
+    """
+    :return:
+    """
+    B_1 = []
+    B_2 = []
+    B_3 = []
+    B_4 = []
+    B_5 = []
+    B_6 = []
+    x_t = []
+    x_temp = []
 
+    #Add the input later
+    for j in range(0,k):
+        if j == 0:
+            x_t = [0 * element for element in range(0, n)]
+            x_t = np.array(x_t)
+            x_temp = x_t
+        f_B(B_1, A, B, x_temp, n) # Setting array B_1
 
-"""
-def one_output_step():
-    while x < xend :
-        if xend - x < h :
-            h = xend - x
-        RungeKutta(x, y, n, h)
+        for i in range (0,n):
+            x_temp[i]=x_t[i] + get_h()*B_1[i]
+        f_B(B_2, A, B, n) # Setting array B_2
 
+        for i in range(0, n): # Setting array B_3
+            x_temp[i] = x_t+ (get_h()/2) * B_1[i] + (get_h()/2) * B_2[i]
+        f_B(B_3, A, B, x_temp, n) # Setting array B_3
 
-def derivative(y, t, k): 
-    # For a model dy/dt = -k * y(t)
-    dydt = -k * y
-    return dydt
+        for i in range(0, n): # Setting array B_4
+            x_temp[i] = x_t[i] + 2*get_h()*B_3[i]
+        f_B(B_4, A, B, x_temp, n) # Setting array B_
 
-# initial condition
-y0 = 5
+        for i in range(0, n): # Setting array B_5
+            x_temp[i] = x_t[i] + (get_h()/12)*(5*B_1[i]+8*B_3[i]-B_4[i])
+        f_B(B_5, A, B, x_temp, n)  # Setting array B_5
 
-# time points
-t = np.linspace(0, 20)
-
-#def reduce_order(model, ):
-    
-def RungeKutta(x0, y0, x, h): 
-    n = (int)((x - x0)/h)
-    y = y0 
-    for i in range(1, n + 1): 
-        k1 = h * dydx(x0, y) 
-        k2 = h * dydx(x0 + 0.5 * h, y + 0.5 * k1) 
-        k3 = h * dydx(x0 + 0.5 * h, y + 0.5 * k2) 
-        k4 = h * dydx(x0 + h, y + k3) 
- 
-        y = y + (1.0 / 6.0)*(k1 + 2 * k2 + 2 * k3 + k4) 
-        x0 = x0 + h 
-    return y 
-
-x0 = 0
-y = 1
-x = 2
-h = 0.2
-print(rungeKutta(x0, y, x, h))
-
-"""
+        for i in range(0, n):
+            x_temp[i] = x_t[i] +(get_h()/3)*(B_1 + B_4 + B_5)
+        f_B(B_6, A, B, x_temp, n)  # Setting array B_6
