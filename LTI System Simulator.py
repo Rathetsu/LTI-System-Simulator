8aa217961e2118953f64dc9a476b2e910bb3257b
Rathetsu <ah.ezzat.muh@gmail.com>
2020-05-27 17:19:25 +0200

    [insert meaningful commit message here]

--- LTI System Simulator.py
+++ LTI System Simulator.py
@@ -10,6 +10,8 @@ from tkinter import *
 import SSR
 #import Solver
 
+HEIGHT = 500
+WIDTH = 700
 
 def create_label_widget(window, var_name, r, c):
     label_widget = Label(window, text = var_name, font = ("Times New Roman", 20), fg = 'white', bg='#1C1C1C', bd = 6)
@@ -24,21 +26,36 @@ def create_entry_widget(window, r, c):
 
 root = Tk()
 root.title("LTI System Simulator")
-root.geometry("700x500")
-root.configure(bg='#1C1C1C')
-img = PhotoImage(file = 'UI assets/title.png')
-title = Label(root, image = img, bg='#1C1C1C')
+#img = PhotoImage(file = 'UI assets/title.png')
+#root.geometry("700x500")
+#root.configure(bg='#1C1C1C')
 
-# root widgets placements
-root.grid_columnconfigure(0, minsize = 30)
-title.grid(row = 0, column = 1 )
-root.grid_rowconfigure(1, minsize = 50)
+canvas = Canvas(root, height = HEIGHT, width = WIDTH )
+canvas.pack()
 
-n_input = Entry(root, width = 10, font = ("Times New Roman", 20), fg = 'black', bd = 6)
-m_input = Entry(root, width = 10, font = ("Times New Roman", 20), fg = 'black', bd = 6)
+#title = Label(root, image = img, bg='#1C1C1C')
+#title.pack(side = TOP)
 
-#n_input.pack(side = LEFT, pady = 15, padx = 25)
-#m_input.pack(side = LEFT, pady = 15, padx = 25)
+frame1 = Frame(root, bg = '#1C1C1C')
+frame1.place(relwidth = 1, relheight = 1)
+
+#root.grid_columnconfigure(0, minsize = 20)
+#title.grid(row = 0, column = 1 )
+#root.grid_rowconfigure(1, minsize = 50)
+
+n_label = Label(frame1, text = "n = ", font = ("Georgia", 22), fg = 'white', bg='#1C1C1C')
+m_label = Label(frame1, text = "m = ", font = ("Georgia", 22), fg = 'white', bg='#1C1C1C')
+n_input = Entry(frame1, width = 5, font = ("Times New Roman", 20), fg = 'black', bd = 6)
+m_input = Entry(frame1, width = 5, font = ("Times New Roman", 20), fg = 'black', bd = 6)
+
+frame1.grid_rowconfigure(0, minsize = 200)
+frame1.grid_columnconfigure(0, minsize = 30)
+n_label.grid(row = 1, column = 1)
+n_input.grid(row = 1, column = 2)
+frame1.grid_columnconfigure(3, minsize = 80)
+m_label.grid(row = 1, column = 4)
+m_input.grid(row = 1, column = 5)
+frame1.grid_rowconfigure(2, minsize = 30)
 
 
 parameter_entry_widgets = []
@@ -46,7 +63,6 @@ a = []
 b = [0]
  # The zero is a place holder that we clear later in the event of calling the parameters_window function.
  # Its purpose is to bypass the IndexError "list index out of range".     
-
 
 def parameters_window():
     new_window = Toplevel(root)
@@ -117,9 +133,8 @@ def parameters_window():
 
     new_window.mainloop()
 
-parameters_button = Button(root, text = "Input & Output Parameters", bg = 'white', fg = 'black', font =("Georgia", 15, 'bold'), bd = 6, command = parameters_window)
-#parameters_button.pack()
-
+parameters_button = Button(frame1, text = "Input & Output Parameters", bg = 'white', fg = 'black', font =("Georgia", 15, 'bold'), bd = 6, command = parameters_window)
+parameters_button.grid(row = 3, column = 5)
 
 #signature = Label(root, text = "Created by")
 
