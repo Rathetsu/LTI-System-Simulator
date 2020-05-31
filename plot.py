72efb73a58150a49b6ec13fbf70ad7af0633b9b6
Rathetsu <ah.ezzat.muh@gmail.com>
2020-05-31 16:21:38 +0200

    wryyyy

--- plot.py
+++ plot.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy
 
-def generate_input(type, t, k):
+def generate_input(type, t: float, k: float):
     """
     parameter type can either be 1 or 2 to choose signal type.
     parameter t has to be a numerical value representing the shift amount
     parameter k is the total number of time steps excluding the last step.
     returns a list u containing the y axis values of the function. 
-
     """
+    t = int(t * 100)
+    k = int(k * 100)
     u = [0] * k
+
     shift = abs(t) * ( t < 0 )
 
     if type == 1:   # Unit Step
@@ -20,3 +22,5 @@ def generate_input(type, t, k):
     
     return u
 
+
+print(generate_input(1, 0.01 , 10))
