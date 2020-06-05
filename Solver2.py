53e241ba8655e143eed08a30e42177668970cc49
Omar <omarmuhammed1998.om@gmmail.com>
2020-06-05 03:38:46 +0200

    Solver2 is 100% done

--- Solver2.py
+++ Solver2.py
@@ -4,27 +4,34 @@ import SSR
 import plot
 
 
-n = 2
-m = 1
-a = [1, 4, 25]
-b = [50]
+n = 4
+#m = 0
+#a = [1, 4, 25]
+#b = [50]
 
-A = SSR.setA(a)
-B = SSR.setB(n)
-C = SSR.setC(a, b)
-D = SSR.setD(n, m, a, b)
-
+#A = SSR.setA(a)
+#B = SSR.setB(n)
+#C = SSR.setC(a, b)
+#D = SSR.setD(n, m, a, b)
 
 #A = np.array([[-7]])
 #B = np.array([[1]])
 #C = np.array([-20])
 #D = np.array([4])
-#A = np.array([[0, 1], [-2, -3]])
-#B = np.array([[0], [5]])
-#A = np.array([[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [-0.125, -0.125, -0.375, -0.125]])
-#B = np.array([[0], [0], [0], [1]])
-#C = np.array([0.125, 1.25, 0.5, 0.875] )
-#
+
+#A = np.array([[0, 1, 0], [0, 0, 1], [-2, -4, -1]])
+#B = np.array([[0], [0], [1]])
+#C = np.array([2, -4, 0])
+#D = np.array([2])
+
+
+
+
+A = np.array([[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [-3, -5, -4, -3]])
+B = np.array([[0], [0], [0], [1]])
+C = np.array([9, 0, 0, 0] )
+D = np.array([0])
+
 
 
 def step_h(tk=10, k=1000):
@@ -32,12 +39,12 @@ def step_h(tk=10, k=1000):
     return h
 
 
-def func_B(x, u_type, A, B):
+def func_B(x, u,A, B):
     """
 
     :return:
     """
-    B_i = A.dot(x) + B.dot(u_type) #replace (1) with the input
+    B_i = A.dot(x) + B.dot(u) #replace (1) with the input
     return B_i
 
 
@@ -62,13 +69,14 @@ def SS_SSEB(n, u_type, k=1000, tk=10):
     t = []
     u = 1
     y_t = []
+    xh_s = np.zeros(n * (k+3)).reshape(n, k+3)
 
     for j in range(0, k+1, 2):
 
 
         #Getting B1
         x_temp = x_t
-        B1 = func_B(x_temp, u, A, B)
+        B1 = func_B(x_temp, u,A, B)
 
         #Getting B2
         x_temp = x_t + step_h(tk, k) * B1
@@ -76,7 +84,7 @@ def SS_SSEB(n, u_type, k=1000, tk=10):
 
         #Getting B3
         x_temp = x_t + (step_h(tk, k)/2) * B1 + (step_h(tk, k)/2) * B2
-        B3 = func_B(x_temp, u, A, B)
+        B3 = func_B(x_temp, u,A, B)
 
         #Getting B4
         x_temp = x_t + 2 * step_h(tk, k) * B3
@@ -88,7 +96,7 @@ def SS_SSEB(n, u_type, k=1000, tk=10):
 
         #Getting B6
         x_temp = x_t + (step_h(tk, k) / 3) * (B1 + B4 + 4 * B5)
-        B6 = func_B(x_temp, 1, A, B)
+        B6 = func_B(x_temp, u, A, B)
 
 
         #Getting x(j+1) and x(j+2)
@@ -102,16 +110,24 @@ def SS_SSEB(n, u_type, k=1000, tk=10):
         #x_j2 ==> x_t
         x_t = x_j2
 
+        """
         if u_type == 1:
             u = 1
         else:
             u = 0
-
+        """
     for j in range(k+3):
         #A list that contains all the time samples
         t = np.append(t, j * step_h(tk, k))
-    print(x_s.shape)
-    y_t = C.dot(x_s) + D.dot(plot.generate_input(u_type))
+
+    for i in range(n):
+        for j in range(k+2):
+            xh_s[i][j] = (x_s[i][j+1] - x_s[i][j]) / step_h(tk, k)
+
+    if u_type == 1:
+        y_t = C.dot(x_s) + D.dot(1)
+    elif u_type == 2:
+        y_t = C.dot(xh_s) + D.dot(0)
 
 
     print(x_t)
@@ -124,12 +140,12 @@ def SS_SSEB(n, u_type, k=1000, tk=10):
     print(B6)
     print(x_j1)
     print(x_j2)
-    print(x_s.shape)
-
-    print(y_t)
+    print(x_s)
+    print(xh_s)
+    print(y_t[0])
 
     plt.plot(t, y_t)
     plt.grid(True)
     plt.show()
 
-SS_SSEB(n, 2,1000, 10)
+SS_SSEB(n, 1,1000, 200)
